1c405ce9a58385aba58e4cf1e4a1d0e09ebc509f
honghai <honghai@dian.so>
2021-11-12 18:01:42 +0800

    add color

--- src/main/java/io/moyada/feign/help/support/Printer.java
+++ src/main/java/io/moyada/feign/help/support/Printer.java
@@ -9,9 +9,32 @@ import javax.tools.Diagnostic;
  **/
 public class Printer {
 
-    private static final String INFO_TAG = "[INFO] ";
-    private static final String WARNING_TAG = "[WARNING] ";
-    private static final String ERROR_TAG = "[ERROR] ";
+    /**
+     *
+     # LOG_CLRSTR_NONE = "\033[m"
+     # LOG_CLRSTR_RED = "\033[0;32;31m"
+     # LOG_CLRSTR_GREEN = "\033[0;32;32m"
+     # LOG_CLRSTR_YELLOW = "\033[1;33m"
+     # LOG_CLRSTR_CYAN = "\033[0;36m"
+     # LOG_CLRSTR_BROWN = "\033[0;33m"
+     # LOG_CLRSTR_WHITE = "\033[1;37m"
+     */
+
+    private static final String TAG_RED     =   "\033[31;4m";
+    private static final String TAG_GREEN   =   "\033[32;4m";
+    private static final String TAG_YELLOW  =   "\033[33;4m";
+    private static final String TAG_CYAN    =   "\033[34;4m";
+    private static final String TAG_END     =   "\033[0m";
+
+    private static final String INFO_TAG    =   "["+TAG_CYAN+"INFO"+TAG_END+"] ";
+    private static final String WARNING_TAG =   "["+TAG_YELLOW+"WARNING"+TAG_END+"] ";
+    private static final String ERROR_TAG   =   "["+TAG_RED+"ERROR"+TAG_END+"] ";
+
+    public static void main(String[] args) {
+        System.out.println(INFO_TAG);
+        System.out.println(WARNING_TAG);
+        System.out.println(ERROR_TAG);
+    }
 
     private final Messager messager;
 
